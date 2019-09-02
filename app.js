9a774457c2a56751ce89a5e5c5d233a5254e7d5d
Rob Fuller <fullergalway@gmail.com>
2019-09-02 16:24:21 +0100

    Allow user to configure erddaps and add their private erddaps, addresses issue #7

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var ERDDAP = function(settings){
     this.settings = settings;
 	this.endpoint = settings.url.replace(/\/+$/, "");
+	this.disabledkey =  this.endpoint+".disabled";
+	this.settings.disabled = localStorage.getItem(this.disabledkey)?true:false;
+}
+
+ERDDAP.prototype.setDisabled = function(disabled){
+		this.settings.disabled = disabled;
+		if(disabled){
+			localStorage.setItem(this.disabledkey,"true");
+		}else{
+			localStorage.removeItem(this.disabledkey);
+		}
 }
 
 var e2o = function(data){
@@ -61,6 +72,72 @@ ERDDAP.prototype.testConnect = function(){
 				resolve(false);
 			});
 	}.bind(this));
+}
+ERDDAP.prototype.settingstr = function(container,index){
+	var tr = document.createElement("tr");
+//	var indextd = document.createElement("td");
+//	indextd.appendChild(document.createTextNode((index+1)+(".")))
+//	tr.appendChild(indextd);
+	var connecttd = document.createElement("td");
+	var cb = document.createElement("input");
+	cb.setAttribute("type","checkbox");
+	var setTitle = function(cb){
+		var connection_status = this.settings.connected?"Connection OK": "Connection test failed";
+		var disabled_status = this.settings.disabled?", search disabled by user.":  this.settings.connected? ", search enabled.": ".";
+		cb.setAttribute("title",connection_status+disabled_status);
+	}.bind(this);
+	var connected = this.settings.connected;
+	if(connected){
+		if(!this.settings.disabled){
+			cb.setAttribute("checked",true);
+		}
+	}else{
+		cb.setAttribute("disabled",true);
+	}
+	setTitle(cb);
+	var erddap = this;
+	cb.onclick = function(){
+		erddap.setDisabled(!this.checked);
+		setTitle(this);
+	};
+	var cblabel = document.createElement("label");
+	cblabel.appendChild(cb);
+	cblabel.appendChild(document.createTextNode(" "+(index+1)+(".")));
+	var cbdiv = document.createElement("div");
+	cbdiv.setAttribute("class","checkbox");
+	cbdiv.appendChild(cblabel);
+	var cbform = document.createElement("div");
+	cbform.setAttribute("class","form-inline");
+	cbform.appendChild(cbdiv);
+	connecttd.appendChild(cbform);
+	tr.appendChild(connecttd);
+	var edit = document.createElement("td");
+	if(erddap.settings.custom){
+		var a = document.createElement("a");
+		a.setAttribute("href","#delete")
+		a.setAttribute("title","Delete this server from the list");
+		a.setAttribute("class","btn btn-default");
+		var trash = document.createElement("i");
+		trash.setAttribute("class","fa fa-trash");
+		a.onclick=function(){
+			erddap.deleted = true;
+			container.showServersList();
+			return false;
+		}
+		a.appendChild(trash);
+		a.appendChild(document.createTextNode("Delete"));
+		edit.appendChild(a);
+	}
+	tr.appendChild(edit);
+	var name = this.settings.name;
+	var nametd = document.createElement("td");
+	nametd.appendChild(document.createTextNode(name));
+	tr.appendChild(nametd);
+	var url = this.settings.url;
+	var urltd = document.createElement("td");
+	urltd.appendChild(document.createTextNode(url));
+	tr.appendChild(urltd);
+	return tr;
 }
 
 var addLinks = function(text){
@@ -327,29 +404,76 @@ var setSearchStatus = function(text){
 var clearSearchResults = function(info){
 	document.getElementById("searchResults").innerHTML = "";
 }
+var filterServers = function(){
+	var table = document.getElementById("settingsTable");
+	if(table){
+		var filters = document.getElementById("filter").value.split(/\s+/).map(function(t){return t.toLowerCase();});
+		var rows = table.rows;
+		var n = table.rows.length;
+		for(var i=1;i<n;i++ ){//skip header row
+			if(filters.length){
+				var include = true;
+				var text = rows[i].innerText.toLowerCase();
+				for(var j=0;j<filters.length;j++){
+					if(text.indexOf(filters[j])<0){
+						include = false;
+						break;
+					}
+				}
+				rows[i].style.display = include?"table-row":"none";
+				rows[i].erddap.visible = include;
+			}else{
+				rows[i].style.display = "table-row";
+
+			}
+		}
+	}
+}
 var clearFilterServers = function(){
 	var filter = document.getElementById("filter");
 	filter.value = "";
 	filter.focus();
+	filterServers();
 }
 var clearSearchDatasets = function(){
     setSearchStatus("");
 	clearSearchResults();
+	history.pushState("", document.title, window.location.pathname
+                                                       + window.location.search);
 }
-var ERDDAPs = function(configs){
+var ERDDAPs = function(configs,includeCustomConfigs){
     this.erddaps = configs.map(function(e){return new ERDDAP(e)});
+    if(includeCustomConfigs){
+    	var customErddaps = this.getCustomConfigs().map(function(e){return new ERDDAP(e)});
+    	for(var i=customErddaps.length-1;i>=0;i--){
+    		var include = true;
+    		for(var j=0;j<this.erddaps.length; j++){
+    			if(this.erddaps[j].endpoint == customErddaps[i].endpoint){
+    				include = false;
+    				break;
+    			}
+    		}
+    		if(include){
+    			this.erddaps.unshift(customErddaps[i]);
+    		}
+    	}
+    	this.saveCustomConfigs();
+    }
     this.searchId = 0;
 }
 ERDDAPs.prototype.testConnect = function(){
 	var nerddaps = this.erddaps.length;
 	var remaining = nerddaps;
+	document.getElementById("testConnections").style.display='block';
 	var showTestConnectionStatus = function(){
 		document.getElementById("testConnections").innerText = "Testing "+nerddaps+" ERDDAP connections, waiting for "+ --remaining;
 	}
 	    var promises = this.erddaps.map(function(erddap){return erddap.testConnect().then(function(){
 	    	showTestConnectionStatus();
 	    })});
-	    return Promise.all(promises);
+	    return Promise.all(promises).then(function(){
+	    				document.getElementById("testConnections").style.display='none';
+	    });
 }
 ERDDAPs.prototype.search = function(query){
 	clearSearchResults("");
@@ -362,7 +486,7 @@ ERDDAPs.prototype.search = function(query){
 		history.replaceState(null, null, document.location.pathname + '#'+urlParams.toString());
 		var erddaps = this.erddaps.filter(function(erddap){
 			//console.log(erddap);
-			return erddap && erddap.settings && erddap.settings.connected;
+			return erddap && erddap.settings && erddap.settings.connected && !erddap.settings.disabled;
 		});
         var nsearches = erddaps.length;
         var nerddaps = nsearches;
@@ -414,4 +538,124 @@ ERDDAPs.prototype.search = function(query){
             });
         }.bind(this));
 	}
+}
+ERDDAPs.prototype.settingstable = function(){
+	this.erddaps = this.erddaps.filter(function(erddap){return !erddap.deleted});
+	var table = document.createElement("table");
+	table.setAttribute("id","settingsTable");
+	table.setAttribute("class","table");
+	var thead = document.createElement("thead");
+	var tr = document.createElement("tr");
+	var th = document.createElement("th");
+	var cb = document.createElement("input");
+	cb.setAttribute("type","checkbox");
+	th.appendChild(cb);
+	tr.appendChild(th);
+	th = document.createElement("th");
+	tr.appendChild(th);
+	th = document.createElement("th");
+	th.appendChild(document.createTextNode("Name"));
+	tr.appendChild(th);
+	th = document.createElement("th");
+	th.appendChild(document.createTextNode("Url"));
+	tr.appendChild(th);
+	th = document.createElement("th");
+	tr.appendChild(th);
+	thead.appendChild(tr);
+	table.appendChild(thead);
+	var tbody = document.createElement("tbody");
+	for(var i=0;i<this.erddaps.length;i++){
+		tr = this.erddaps[i].settingstr(this,i);
+		this.erddaps[i].tr = tr;
+		this.erddaps[i].visible = true;
+		tr.erddap = this.erddaps[i];
+		tbody.appendChild(tr);
+	}
+	cb.setAttribute("title","Click to enable/disable listed servers");
+	cb.onclick = function(){
+		console.log("clicked")
+		var rows = table.rows;
+		var n = table.rows.length;
+		for(var i=1;i<n;i++ ){//skip header row
+			var erddap = rows[i].erddap;
+			if(erddap.visible && erddap.settings.connected){ // hmmn could just use css selector...
+				rows[i].querySelector("input").checked = this.checked;
+				erddap.setDisabled(!this.checked);
+			}
+		}
+
+	}
+	table.appendChild(tbody);
+	return table;
+}
+function isValidURL(str) {
+	//https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+   var a  = document.createElement('a');
+   a.href = str;
+   return (a.host && a.host != window.location.host);
+}
+ERDDAPs.prototype.getCustomConfigs = function(){
+	return JSON.parse(localStorage.getItem("custom") || "[]");
+}
+ERDDAPs.prototype.saveCustomConfigs = function(){
+	localStorage.setItem("custom",JSON.stringify(this.erddaps.filter(function(k){return k.settings.custom && !k.deleted;}).map(function(e){return e.settings;})));
+}
+ERDDAPs.prototype.showServersList = function(){
+		document.getElementById("erddapServers").innerHTML= "";
+		document.getElementById("erddapServers").appendChild(this.settingstable());
+		this.saveCustomConfigs()
+		filterServers();	
+}
+ERDDAPs.prototype.addNewServerFromForm = function(){
+	var urlhelp = document.getElementById("newServerUrlHelpBlock");
+	urlhelp.innerText = "";
+	var urlel = document.getElementById("newServerUrl");
+	var url = urlel.value.trim();
+	if(url.length == 0){
+		hideAddServerForm();
+		return;
+	}
+	if(!isValidURL(url)){
+		urlhelp.innerText = "Not a valid URL";
+		return;
+	}
+	var name = document.getElementById("newServerName").value.trim();
+	var e2 = new ERDDAPs([{name: name, url: url, custom: true}]);
+	for(var i=0;i<this.erddaps.length; i++){
+		if(e2.erddaps[0].endpoint == this.erddaps[i].endpoint){
+			urlhelp.innerText = "Server is already listed";
+			return;
+		}
+	}
+	e2.testConnect().then(function(){
+		this.erddaps.unshift(e2.erddaps[0]);
+		this.showServersList();
+		hideAddServerForm();
+	}.bind(this));
+
+}
+
+var showAddServerForm = function(){
+	document.getElementById("newServerUrlHelpBlock").innerText = "";
+	document.getElementById("newServerUrl").value = "";
+	document.getElementById("newServerName").value = "";
+	document.getElementById("addServerForm").style.display = "block";
+	document.getElementById("configureErddapServer").style.display = "none";
+	document.getElementById("erddapServers").style.display = "none";
+	document.getElementById("newServerName").focus();
+}
+var hideAddServerForm = function(){
+	document.getElementById("addServerForm").style.display = "none";
+	document.getElementById("configureErddapServer").style.display = "block";
+	document.getElementById("erddapServers").style.display = "block";
+}
+
+var showSettings = function(){
+	document.getElementById("searchArea").style.display = "none";
+	document.getElementById("configurationArea").style.display = "block";
+}
+var showSearch = function(){
+	hideAddServerForm();
+	document.getElementById("configurationArea").style.display = "none";
+	document.getElementById("searchArea").style.display = "block";
 }
